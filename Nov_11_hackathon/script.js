0f6326ec8fef580424d46907c99a6131c02aef42
niveditaganesh <55006494+niveditaganesh@users.noreply.github.com>
2020-11-11 18:17:50 +0530

    Add files via upload

---
+++ Nov_11_hackathon/script.js
@@ -0,0 +1,9 @@
+function selectModal(){
+    var selectvalue=document.getElementById('newform').value;
+    if(selectvalue=="NEWTICKET"){
+        alert('new tickettttt')
+    }
+}
+function redirectpage() {
+    location.replace("index.html")
+  }
