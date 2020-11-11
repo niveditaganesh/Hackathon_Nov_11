2c29c6d42e22e87c6c434faf3cfd5f6e8b9a5310
niveditaganesh <55006494+niveditaganesh@users.noreply.github.com>
2020-11-11 19:04:55 +0530

    Delete script.js

--- Nov_11_hackathon/script.js
+++
@@ -1,9 +0,0 @@
-function selectModal(){
-    var selectvalue=document.getElementById('newform').value;
-    if(selectvalue=="NEWTICKET"){
-        alert('new tickettttt')
-    }
-}
-function redirectpage() {
-    location.replace("index.html")
-  }
